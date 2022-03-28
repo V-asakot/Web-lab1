0465fa7b09bfce6e4188ddd90947fa4a22c6794f
vasilii-kotkov <v.kotov@pampadu.ru>
2022-03-28 17:46:58 +0300

    example commit 2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, response) => {
 })
 
 app.post("/", urlencodedParser, function (request, response) {
-  if(!request.body) return response.sendStatus(400);
+  if(!request.body) return response.sendStatus(600);
   console.log(request.body);
 
   var xhr = new XMLHttpRequest();
